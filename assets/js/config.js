dbef9c426020bae84d0a8f6719a11b16b7e9d512
SoporteTI-desing <soporte-ti@innvida.es>
2025-11-02 20:13:30 -0600

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+// assets/js/config.js
+// Configuración Firebase para CRM INNVIDA (crm-innvida-76e2e)
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAyksNhyRX-7QnZSOF27txNU-_SeMoOGps",
+  authDomain: "crm-innvida-76e2e.firebaseapp.com",
+  projectId: "crm-innvida-76e2e",
+  storageBucket: "crm-innvida-76e2e.firebasestorage.app",
+  messagingSenderId: "865341286325"
+  // appId: "1:865341286325:web:XXXXXXXXXXXXXX" // opcional
+};
